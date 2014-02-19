bee7fff4e45431f274d50913397f825404cbed0e
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2014-02-19 23:32:05 +0100

    Set pet name in action bar

--- App/GooglyZooApp/src/main/java/fr/tvbarthel/attempt/googlyzooapp/MainActivity.java
+++ App/GooglyZooApp/src/main/java/fr/tvbarthel/attempt/googlyzooapp/MainActivity.java
@@ -110,20 +110,6 @@ public class MainActivity extends Activity
         releaseCamera();
     }
 
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
-    }
-
     public void restoreActionBar() {
         ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
@@ -251,6 +237,7 @@ public class MainActivity extends Activity
     @Override
     public void onNavigationDrawerItemSelected(GooglyPet petSelected) {
         mGooglyPetView.setModel(petSelected);
+        mTitle = getResources().getString(petSelected.getNameId());
     }
 
 
