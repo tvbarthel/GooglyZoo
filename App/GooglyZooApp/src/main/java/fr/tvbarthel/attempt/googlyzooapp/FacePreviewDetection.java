aee8e850df335f7c134efd03d351729b2316a449
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-03-30 15:22:22 +0200

    Delete code that might be useless. (Tested on my HTC one S and my N5)

--- App/GooglyZooApp/src/main/java/fr/tvbarthel/attempt/googlyzooapp/FacePreviewDetection.java
+++ App/GooglyZooApp/src/main/java/fr/tvbarthel/attempt/googlyzooapp/FacePreviewDetection.java
@@ -6,8 +6,6 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-import java.io.IOException;
-
 /**
  * Created by tbarthel on 06/02/14.
  */
@@ -15,7 +13,6 @@ public class FacePreviewDetection extends SurfaceView implements SurfaceHolder.C
 
     private static final String TAG = FacePreviewDetection.class.getName();
 
-    private SurfaceHolder mHolder;
     private Camera mCamera;
 
 
@@ -30,8 +27,10 @@ public class FacePreviewDetection extends SurfaceView implements SurfaceHolder.C
 
         // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed.
-        mHolder = getHolder();
-        mHolder.addCallback(this);
+        SurfaceHolder surfaceHolder = getHolder();
+        if (surfaceHolder != null) {
+            surfaceHolder.addCallback(this);
+        }
 
         mCallback = callback;
     }
@@ -41,8 +40,9 @@ public class FacePreviewDetection extends SurfaceView implements SurfaceHolder.C
         try {
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
-        } catch (IOException e) {
-            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+            startFaceDetection();
+        } catch (Exception e) {
+            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
         }
     }
 
@@ -51,30 +51,8 @@ public class FacePreviewDetection extends SurfaceView implements SurfaceHolder.C
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        // If your preview can change or rotate, take care of those events here.
-        // Make sure to stop the preview before resizing or reformatting it.
-
-        if (mHolder.getSurface() == null) {
-            // preview surface does not exist
-            return;
-        }
-
-        // stop preview before making changes
-        try {
-            mCamera.stopPreview();
-        } catch (Exception e) {
-            // ignore: tried to stop a non-existent preview
-        }
-
-        // start preview with new settings
-        try {
-            mCamera.setPreviewDisplay(mHolder);
-            mCamera.startPreview();
-            startFaceDetection();
-
-        } catch (Exception e) {
-            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
-        }
+        // Since we don't need to adapt anything to structural changes of the surface,
+        // we do nothing here.
     }
 
     private void startFaceDetection() {
